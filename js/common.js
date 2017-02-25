dba5b7a961d69ac65d53c99d99f819eb77b145e7
yyuxiang <18916383119@163.com>
2017-02-25 14:05:29 +0800

    个站

---
+++ js/common.js
@@ -0,0 +1,122 @@
+//removeClass
+function removeClass(obj,sClass){
+	var reg=new RegExp('\\b'+sClass+'\\b','g');
+	if(reg.test(obj.className)){
+		obj.className=obj.className.replace(reg,'').replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
+	}
+}
+//hasClass
+function hasClass(obj,sClass){
+	var reg=new RegExp('\\b'+sClass+'\\b');
+	if(reg.test(obj.className)){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+//move
+
+function getStyle(obj,name) {
+    return (obj.currentStyle || getComputedStyle(obj,false))[name];
+}
+
+function move(obj,json,options) {
+    options = options || {};
+    options.duration = options.duration || 500;
+    options.easing = options.easing || 'ease-out';
+
+    // 先关后开
+    clearInterval(obj.timer);
+    var start = {}; // 起点
+    var dis = {}; // 总距离
+    for (var name in json) {
+        start[name] = parseFloat(getStyle(obj,name));
+        dis[name] = json[name] - start[name];
+    }
+    // 总次数
+    var count = Math.floor(options.duration/30);
+    var n = 0; // 当前走了多少次了
+    obj.timer = setInterval(function(){
+        n++;
+
+        for (var name in json) {
+            switch(options.easing) {
+                case 'linear':
+                    var a = n / count;
+                    var cur = start[name] + dis[name] * a;
+                    break;
+                case 'ease-in':
+                    var a = n / count;
+                    var cur = start[name] + dis[name] * Math.pow(a,3);
+                    break;
+                case 'ease-out':
+                    var a = 1 - n/count;
+                    var cur = start[name] + dis[name] * (1 - Math.pow(a,3));
+                    break;
+            }
+            if (name == 'opacity') {
+                obj.style.opacity = cur;
+                obj.style.filter = 'alpha(opacity'+cur*100+')';
+            } else {
+                obj.style[name] = cur + 'px';
+            }
+        }
+        // 走的次数和总次数相等 不能继续走
+        if (n == count) {
+            // 关闭定时器
+            clearInterval(obj.timer);
+            options.complete && options.complete();
+        }
+    },30);
+}
+//求绝对距离
+function getPos(obj){
+	var l=0;
+	var t=0;
+	while(obj){
+		l+=obj.offsetLeft;
+		t+=obj.offsetTop;
+		obj=obj.offsetParent;
+	}
+	return {left:l,top:t};
+}
+
+//jsonP
+function jsonp(options){
+    //整理options
+    options=options||{};
+    options.data=options.data||{};
+    options.cbKey=options.cbKey||'cb';
+    options.timeout=options.timeout||0;
+
+    options.data[options.cbKey]=('jsonp'+Math.random()).replace('.','');
+    window[options.data[options.cbKey]]=function(json){
+        clearTimeout(timer);
+        options.success && options.success(json);//数据回来了
+        //清理操作
+        document.getElementsByTagName('head')[0].removeChild(oSc);//用过的script标签，清除
+        window[options.data[options.cbKey]]=null;//用过的全局函数清空
+    };
+    //整理data
+    var arr=[];
+    for(var key in options.data){
+        arr.push(key+'='+encodeURIComponent(options.data[key]));
+    }
+    var str=arr.join('&');
+    //创建script标签,丢到页面
+    var oSc=document.createElement('script');
+    oSc.src=options.url+'?'+str;
+    document.getElementsByTagName('head')[0].appendChild(oSc);
+    if(options.timeout){
+        var timer=setTimeout(function(){
+            options.error && options.error();
+            //超时了也要清除用过的标签，和随机函数
+            window[options.data[options.cbKey]]=function(json){
+                //清理操作
+                document.getElementsByTagName('head')[0].removeChild(oSc);//用过的script标签，清除
+                window[options.data[options.cbKey]]=null;//用过的全局函数清空
+            };
+        },options.timeout);
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,169 @@
+window.onload=function(){
+//		头部
+		var oHead=document.getElementById("head");
+		var oDiv=document.getElementById("right");
+		var oImg=oHead.getElementsByTagName('img')[0];
+		var aImg=oHead.getElementsByTagName('img');
+		var docW=document.documentElement.clientWidth;
+		var docH=document.documentElement.clientHeight;
+		
+		if(docW>1000){
+			n=docW/5;
+		}else{
+			n=docW/100;
+		}
+		move(oImg,{left:n},{
+			duration:1500,
+			complete:function(){
+				move(oImg,{top:50},{
+					duration:1000
+				})
+			}
+		});
+		
+		//天气
+		jsonp({
+			url:'http://api.asilu.com/weather/',
+			data:{
+				city:'上海'
+			},
+			cbKey:'callback',
+			success:function(json){
+				var arr=json.weather;
+				var oWeather=document.getElementById("weather");
+				for (var i = 0; i < 4; i++) {
+					var oLi=document.createElement('li');
+					switch (i){
+						case 0:
+							oLi.innerHTML=arr[0].date;
+							break;
+						case 1:
+							oLi.innerHTML=arr[0].weather;
+							break;
+						case 2:
+							oLi.innerHTML=arr[0].wind;
+							break;
+						case 3:
+							oLi.innerHTML=arr[0].temp;
+							break;
+					}
+					oWeather.appendChild(oLi);
+				}
+			}
+		});
+		
+//		主要技能/自我评价
+		var oAboutM=document.getElementById("aboutMe_middle");
+		var aBtn=oAboutM.getElementsByTagName('span');
+		var oAboutT=document.getElementById("aboutMe_top");
+		var oAboutFont=document.getElementById("aboutMe_font")
+		var aUl=oAboutT.getElementsByTagName('ul');
+		var aStrong=oAboutM.getElementsByTagName('strong');
+		
+		for (var i = 0; i < aBtn.length; i++) {
+			(function(index){
+				aBtn[i].onclick=function(){
+					for (var i = 0; i < aBtn.length; i++) {
+						removeClass(aBtn[i],'active');
+						removeClass(aStrong[i],'active');
+						aUl[i].style.display='none';
+					}
+					this.className='active';
+					aStrong[index].className='active';
+					aUl[index].style.display='block';
+				};
+			})(i);
+		}
+		
+		//打字效果
+		var str = '你们好，Welcome to my personal station !';
+
+        for (var i = 0; i < str.length; i++) {
+            var oS = document.createElement('span');
+            oS.innerHTML = str.charAt(i);
+            // 添加
+            aboutMe_font.appendChild(oS);
+        }
+        var aSpan = aboutMe_font.children;
+        var i = 0;
+        var timer = setInterval(function(){
+			move(aSpan[i],{opacity: 1});
+            i++;
+            if (i == aSpan.length) {
+                clearInterval(timer);
+            }
+        },100);
+		
+//		上下滚动选项卡
+		var oContainR=document.getElementById("contain_right");
+		var aBtn2=oContainR.getElementsByTagName('li');
+		var oMainCon=document.getElementById("main_contain");
+		var aDiv=oMainCon.children;
+		for (var i = 0; i < aBtn2.length; i++) {
+			(function(index){
+				aBtn2[index].onclick=function(){
+				for (var i = 0; i < aDiv.length; i++) {
+						aBtn2[i].className='';
+				};
+					this.className='active';
+					move(oMainCon,{top:-index*aDiv[0].offsetHeight});
+				}
+			})(i);
+		}
+};
+	//	技能掌握图解
+		function hoverD(obj,ev){
+			var w=obj.offsetWidth;
+			var h=obj.offsetHeight;
+			var x=getPos(obj).left+w/2-ev.clientX;
+			var y=getPos(obj).top+h/2-ev.clientY;
+			return Math.round((Math.atan2(y,x)*180/Math.PI+180)/90)%4;
+		};
+		var oUl=document.getElementById("Skill");
+		var aLi=oUl.children;
+		var aSpan=oUl.getElementsByTagName('span');
+		for (var i = 0; i < aLi.length; i++) {
+			aLi[i].index=i;
+			aLi[i].onmouseenter=function(ev){
+				var oEvent=ev||event;
+				var n=hoverD(aLi[this.index],oEvent);
+				switch (n){
+					case 0:
+						aSpan[this.index].style.left='200px';
+						aSpan[this.index].style.top=0;
+						break;
+					case 1:
+						aSpan[this.index].style.left=0;
+						aSpan[this.index].style.top='200px';
+						break;
+					case 2:
+						aSpan[this.index].style.left='-200px';
+						aSpan[this.index].style.top=0;
+						break;
+					case 3:
+						aSpan[this.index].style.left=0;
+						aSpan[this.index].style.top='-200px';
+						break;
+				}
+				move(aSpan[this.index],{left:0,top:0},{duration:600});
+			};
+			aLi[i].onmouseleave=function(ev){
+				var oEvent=ev||event;
+				var n=hoverD(aLi[this.index],oEvent);
+				switch (n){
+					case 0:
+						move(aSpan[this.index],{left:'200',top:0});
+						break;
+					case 1:
+						move(aSpan[this.index],{left:0,top:'200'});
+						break;
+					case 2:
+						move(aSpan[this.index],{left:'-200',top:0});
+						break;
+					case 3:
+						move(aSpan[this.index],{left:0,top:'-200'});
+						break;
+				}
+			}
+		}
+	
